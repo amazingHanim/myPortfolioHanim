5f41c375214114fafedbc795598e14409bd0d4c5
amazingHanim <chee_up_8282@naver.com>
2022-04-05 00:29:06 +0900

    s
    s

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+/*
 $(function () {
     $frame = $(".frame"),
         $wrapper = $frame.find(".section_wrap"),
@@ -52,6 +53,67 @@ $(function () {
 
     });
 
+    reset();
+
+});
+*/
+$(function () {
+    $frame = $(".frame"),
+        $wrapper = $frame.find(".section_wrap"),
+        $leftBtn = $frame.find(".pageBtns>.left"),
+        $rightBtn = $frame.find(".pageBtns>.right")
+    leftNum = -100; //고정값
+    clickNum = 0; //변동값
+    function reset() {
+        $leftBtn.text("About Me");
+        $rightBtn.text("My Projects");
+    }
+    function show(){
+        $leftBtn.show();
+        $rightBtn.show();
+    }
+    function txtChange(){
+        if(clickNum==-1){
+            $leftBtn.hide();
+            $rightBtn.text("Menu"); 
+        }else if (clickNum==0){
+            reset();
+
+        }else if(clickNum==1){
+            $leftBtn.text("Menu");
+            $rightBtn.text("detail"); 
+
+        }else if(clickNum ==2){
+            $leftBtn.text("My Projects");
+            $rightBtn.hide(); 
+        }
+    }
+
+
+
+    $rightBtn.click(function () {
+        show();
+        if (clickNum < 2) {
+            clickNum++;
+            $wrapper.animate({
+                left: leftNum * (clickNum+1) + "%"
+            }, 300);
+            console.log(clickNum);
+            txtChange();
+        }
+    });
+    $leftBtn.click(function () {
+        show();
+        if (clickNum > -1) {
+            clickNum--;
+            $wrapper.animate({
+                left: leftNum * (clickNum +1) + "%"
+            }, 300);
+            console.log(clickNum);
+            txtChange();
+        }
+
+    });
     reset();
 
 });
