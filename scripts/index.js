fa48eb11db4dc74de0c2d9b96c034df09b41ab3e
amazingHanim <chee_up_8282@naver.com>
2022-04-06 17:15:36 +0900

    ss
    ss

--- scripts/index.js
+++ scripts/index.js
@@ -1,119 +1,104 @@
-/*
 $(function () {
-    $frame = $(".frame"),
-        $wrapper = $frame.find(".section_wrap"),
-        $leftBtn = $frame.find(".pageBtns>.left"),
-        $rightBtn = $frame.find(".pageBtns>.right")
-    // 
-    function show(){
-        $leftBtn.show();
-        $rightBtn.show();
-    }
-    function reset() {
-        $leftBtn.text("About Me");
-        $rightBtn.text("My Projects");
-    }
-    function txtChange(){
-        if(current==100){
-            $leftBtn.hide();
-            $rightBtn.text("Menu"); 
-        }else if (current==0){
-            reset();
 
-        }else if(current==-100){
-            $leftBtn.text("Menu");
-            $rightBtn.text("detail"); 
-
-        }else if(current ==-200){
-            $leftBtn.text("My Projects");
-            $rightBtn.hide(); 
+    function indexSlide() {
+        $frame = $(".frame"),
+            $wrapper = $frame.find(".section_wrap"),
+            $leftBtn = $frame.find(".pageBtns>.left"),
+            $rightBtn = $frame.find(".pageBtns>.right")
+        leftNum = -100; //고정값
+        clickNum = 0; //변동값
+        function reset() {
+            $leftBtn.text("About Me");
+            $rightBtn.text("My Projects");
         }
-    }
-    current = 0
-    next = true;
-    console.log(current)
 
-    $rightBtn.click(function () {
+        function show() {
+            $leftBtn.show();
+            $rightBtn.show();
+        }
+
+        function txtChange() {
+            if (clickNum == -1) {
+                $leftBtn.hide();
+                $rightBtn.text("Menu");
+            } else if (clickNum == 0) {
+                reset();
+
+            } else if (clickNum == 1) {
+                $leftBtn.text("Menu");
+                $rightBtn.text("detail");
+
+            } else if (clickNum == 2) {
+                $leftBtn.text("My Projects");
+                $rightBtn.hide();
+            }
+        }
+
+
+
+        $rightBtn.click(function () {
             show();
-            $wrapper.animate({
-                marginLeft: (current - 100) + "%"
-            }, 300);
-            current = current-100;
-            txtChange();
+            if (clickNum < 2) {
+                clickNum++;
+                $wrapper.animate({
+                    left: leftNum * (clickNum + 1) + "%"
+                }, 300);
+                console.log(clickNum);
+                txtChange();
+            }
+        });
+        $leftBtn.click(function () {
+            show();
+            if (clickNum > -1) {
+                clickNum--;
+                $wrapper.animate({
+                    left: leftNum * (clickNum + 1) + "%"
+                }, 300);
+                console.log(clickNum);
+                txtChange();
+            }
 
-    });
-    $leftBtn.click(function () {
-        show();
-        $wrapper.animate({
-            marginLeft: (current + 100) + "%"
-        }, 300);
-        current = current + 100;
-        console.log(current);
-        txtChange();
-
-    });
-
-    reset();
-
-});
-*/
-$(function () {
-    $frame = $(".frame"),
-        $wrapper = $frame.find(".section_wrap"),
-        $leftBtn = $frame.find(".pageBtns>.left"),
-        $rightBtn = $frame.find(".pageBtns>.right")
-    leftNum = -100; //고정값
-    clickNum = 0; //변동값
-    function reset() {
-        $leftBtn.text("About Me");
-        $rightBtn.text("My Projects");
-    }
-    function show(){
-        $leftBtn.show();
-        $rightBtn.show();
-    }
-    function txtChange(){
-        if(clickNum==-1){
-            $leftBtn.hide();
-            $rightBtn.text("Menu"); 
-        }else if (clickNum==0){
-            reset();
-
-        }else if(clickNum==1){
-            $leftBtn.text("Menu");
-            $rightBtn.text("detail"); 
-
-        }else if(clickNum ==2){
-            $leftBtn.text("My Projects");
-            $rightBtn.hide(); 
-        }
+        });
+        reset();
     }
 
+    function myProject() {
+        var $myPjc = $("section.myPjt"),
+            $content = $myPjc.find(".content"),
+            $artWrap = $content.children(),
+            $article = $artWrap.children();
+
+        $artNum = $article.length;
+        $artSize = $article.innerHeight();
+        console.log($artSize);
 
 
-    $rightBtn.click(function () {
-        show();
-        if (clickNum < 2) {
-            clickNum++;
-            $wrapper.animate({
-                left: leftNum * (clickNum+1) + "%"
-            }, 300);
-            console.log(clickNum);
-            txtChange();
-        }
-    });
-    $leftBtn.click(function () {
-        show();
-        if (clickNum > -1) {
-            clickNum--;
-            $wrapper.animate({
-                left: leftNum * (clickNum +1) + "%"
-            }, 300);
-            console.log(clickNum);
-            txtChange();
-        }
+console.log($content.scrollTop())
 
-    });
-    reset();
+        nowTop = $content.scrollTop();
+        console.log(nowTop)
 
+        // $content.animate({scrollTop:$artSize},500);
+        var num = 0
+        $content.scroll(
+            
+            function(){
+                console.log(nowTop)
+
+                // num++;
+                // console.log(num)
+                // if($nowTop>$artSize){
+
+                // }
+            }
+        )
+
+        // console.log($artSize)
+        // $content.animate({scrollTop:-$artSize},500); 
+
+
+    }
+
+    indexSlide();
+    myProject();
 });
